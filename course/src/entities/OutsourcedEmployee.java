8be8b806981deb5418609072567c3f24f5e896b6
Thaly <139823474+lacatye@users.noreply.github.com>
2024-01-08 17:53:10 -0300

    Add files via upload

---
+++ course/src/entities/OutsourcedEmployee.java
@@ -0,0 +1,29 @@
+package entities;
+
+public class OutsourcedEmployee extends Employee{
+	
+	public double additionalCharge;
+
+	public OutsourcedEmployee() {
+		super();
+	}
+	public OutsourcedEmployee(String name, Integer hours, Double valuePerHour, double additionalCharge) {
+		super(name, hours, valuePerHour);
+		this.additionalCharge = additionalCharge;
+	}
+
+	public double getAdditionalCharge() {
+		return additionalCharge;
+	}
+
+	public void setAdditionalCharge(double additionalCharge) {
+		this.additionalCharge = additionalCharge;
+	}
+	
+	
+	@Override 
+	public Double payment() {
+		return super.payment() + additionalCharge * 1.1;
+	}
+	
+}
